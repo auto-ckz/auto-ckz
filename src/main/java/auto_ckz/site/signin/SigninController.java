2d82e43cf37c2476529402e021d40b2a3baa3222
m-ciesielski <mateusz.jan.ciesielski@gmail.com>
2016-11-02 15:27:50 +0100

    Basic scaffolding

---
+++ src/main/java/auto_ckz/site/signin/SigninController.java
@@ -0,0 +1,13 @@
+package auto_ckz.site.signin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SigninController {
+
+	@RequestMapping(value = "signin")
+	public String signin() {
+        return "signin/signin";
+    }
+}
