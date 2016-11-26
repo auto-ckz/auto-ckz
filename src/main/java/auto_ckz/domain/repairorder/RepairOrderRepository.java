8cc43e0fd7affa6648b24c44380c5e183b132514
m-ciesielski <mateusz.jan.ciesielski@gmail.com>
2016-11-26 14:38:13 +0100

    Expose REST API for RepairOrders

--- src/main/java/auto_ckz/domain/repairorder/RepairOrderRepository.java
+++ src/main/java/auto_ckz/domain/repairorder/RepairOrderRepository.java
@@ -2,13 +2,17 @@ package auto_ckz.domain.repairorder;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.sql.Date;
 import java.util.List;
 
+@RepositoryRestResource
 public interface RepairOrderRepository extends PagingAndSortingRepository<RepairOrder, Long> {
 
     List<RepairOrder> findByClientId(@Param("clientId") Long clientId);
+    List<RepairOrder> findByClientIdAndCarId(@Param("clientId") Long clientId,
+                                             @Param("carId") Long carId);
     List<RepairOrder> findByMemberOfCustomerServiceId(@Param("memberOfCustomerServiceId") Long memberOfCustomerServiceId);
     RepairOrder findByCarId(@Param("carId") Long carId);
     List<RepairOrder> findByDate(@Param("date") Date date);
