04c3721d8a33a846c12ffeb44f20fd46d52e57ab
m-ciesielski <mateusz.jan.ciesielski@gmail.com>
2016-12-04 19:37:24 +0100

    Seed database with initial data

--- src/main/java/auto_ckz/domain/repairorder/RepairOrderController.java
+++ src/main/java/auto_ckz/domain/repairorder/RepairOrderController.java
@@ -1,6 +1,5 @@
 package auto_ckz.domain.repairorder;
 
-import auto_ckz.common.enums.RepairStatus;
 import auto_ckz.domain.car.Car;
 import auto_ckz.domain.repair.Repair;
 import auto_ckz.domain.repair.RepairRepository;
@@ -11,10 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.math.BigDecimal;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -30,38 +25,12 @@ public class RepairOrderController {
 	//TODO: check if User accessing this page is the owner of a car
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public String repairOrderOverview(@PathVariable long id, Model model) {
-		Car testCar = new Car();
-		testCar.setId(1L);
-		testCar.setMake("Polonez");
-		testCar.setModel("Caro");
-		testCar.setVin("1234567890123456789");
+		RepairOrder repairOrder = repairOrderRepository.findOne(id);
+		Car car = repairOrder.getCar();
+		List<Repair> repairs = repairRepository.findByRepairOrderId(id);
 
-		RepairOrder testRepairOrder = new RepairOrder();
-		testRepairOrder.setId(1L);
-		testRepairOrder.setCar(testCar);
-		testRepairOrder.setTotalCost(new BigDecimal(1500));
-
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		try {
-			java.util.Date parsedDate = format.parse("2016-12-03");
-			testRepairOrder.setDate(new java.sql.Date(parsedDate.getTime()));
-		}
-		catch (ParseException e){
-			e.printStackTrace();
-		}
-
-		Repair testRepair = new Repair();
-		testRepair.setRepairOrder(testRepairOrder);
-		testRepair.setDescription("blabla");
-		testRepair.setStatus(RepairStatus.DONE);
-
-
-		//List<RepairOrder> repairOrders = repairOrderRepository.findByCarId(id);
-		List<Repair> repairs = new ArrayList<>();
-		repairs.add(testRepair);
-
-		model.addAttribute("car", testCar);
-		model.addAttribute("repairOrder", testRepairOrder);
+		model.addAttribute("repairOrder", repairOrder);
+		model.addAttribute("car", car);
 		model.addAttribute("repairs", repairs);
 		return "repair_orders/summary";
 	}
---
+++ src/main/resources/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO car (id, make, model, vin, vehiclecheckup, vehiclemileage, year) VALUES (1, 'Polonez', 'Caro', '12345678901234567', true, 30010, 1985);
+
+INSERT INTO client (id, firstName, lastName, phoneNumber) VALUES (1, 'Stefan', 'Klient', '123456789');
+
+INSERT INTO mechanic (id, firstName, lastName, phoneNumber) VALUES (1, 'Stefan', 'Mechanik', '987654321');
+
+INSERT INTO memberofcustomerservice (id, firstName, lastName, phoneNumber) VALUES (1, 'Stefan', 'Obsluga', '111222333');
+
+INSERT INTO repairorder (id, carid, clientid, memberofcustomerserviceid, totalCost, description, date) VALUES (1, 1, 1, 1, 1500, 'Naprawa zawieszenia', '2016-12-04');
+
+INSERT INTO repair (id, repairorderid, mechanicid, description, status) VALUES (1, 1, 1, 'Wymiana amortyzatorów', 'DONE');
+INSERT INTO repair (id, repairorderid, mechanicid, description, status) VALUES (2, 1, 1, 'Wymiana wachaczy', 'IN_PROGRESS');
