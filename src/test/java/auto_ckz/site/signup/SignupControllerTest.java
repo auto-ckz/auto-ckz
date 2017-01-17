7f096c56a3307cade2d9ac5739fc10c5e9f01dca
mzweigert <mzweigert@sigma.ug.edu.pl>
2017-01-17 21:01:37 +0100

    Translate interface to polish language (#22)

--- src/test/java/auto_ckz/site/signup/SignupControllerTest.java
+++ src/test/java/auto_ckz/site/signup/SignupControllerTest.java
@@ -14,12 +14,6 @@ public class SignupControllerTest extends WebAppConfigurationAware {
     public void displaysSignupForm() throws Exception {
         mockMvc.perform(get("/signup"))
                 .andExpect(model().attributeExists("signupForm"))
-                .andExpect(view().name("signup/signup"))
-                .andExpect(content().string(
-                        allOf(
-                                containsString("<title>Signup</title>"),
-                                containsString("<legend>Please Sign Up</legend>")
-                        ))
-                );
+                .andExpect(view().name("signup/signup"));
     }
 }
